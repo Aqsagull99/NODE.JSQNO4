c6bb29b933a4c3db3fb5a3f9c21d1ad4b56bfc7c
Aqsagull99 <159878357+Aqsagull99@users.noreply.github.com>
2024-02-17 00:10:48 +0500

    Add files via upload

---
+++ QNO4.ts
@@ -0,0 +1,6 @@
+// // QNO:4
+// // Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+
+// let authername:string ="Haider Abbas"
+
+// console.log(` ${authername} Be Calm And Be Proud Because You Are Pakistani`)
